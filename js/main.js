760933372d06d84b5c6408b1c85c51de950faac9
Roman Di Placido <romandiplacido.2001@gmail.com>
2022-07-04 18:49:12 -0300

    proyecto

---
+++ js/main.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+ function openFunction(){
+  document.getElementById("menu").style.width="300px";
+  document.getElementById("mainbox").style.marginLeft="300px";
+  document.getElementById("mainbox").innerHTML="Click on Cross Element and Close Menu";
+ }
+function closeFunction(){
+ document.getElementById("menu").style.width="0px";
+ document.getElementById("mainbox").style.marginLeft="0px";
+ document.getElementById("mainbox").innerHTML="&#9776; Open";
+}
+</script>
